e1c68369b5c55af83d90a18c86706e1c69602dcc
Daniil Galitskii <galichmsk1515@gmail.com>
2022-10-31 06:44:45 +0100

    finished users endpoints

--- server/index.js
+++ server/index.js
@@ -1,31 +1,42 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
-const userController = require('./controllers/user-controller');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userController = require("./controllers/user-controller");
 
-const url = "mongodb+srv://root:root@conteyener.vobs3x6.mongodb.net/?retryWrites=true&w=majority";
+const url =
+  "mongodb+srv://root:root@conteyener.vobs3x6.mongodb.net/?retryWrites=true&w=majority";
 const PORT = 4444;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/",(req, res) => {
-    res.send("HELLO");
-})
+app.get("/", (req, res) => {
+  res.send("HELLO");
+});
 
 app.get("/activate/:link", userController.activate);
-app.post("/signin", userController.registration);
+app.get("/users", userController.getUsers);
+app.get("/profile", userController.getMe);
 
-const start = async() => {
-    try {
-        await app.listen(PORT, ()=>{
-            console.log("Server started on " + PORT);
-        })
-        await mongoose.connect(url, ()=>{console.log("DB IS OK")})
-    } catch (error) {
-        console.log(error)
-    }
-}
+app.post("/auth/signin", userController.registration);
+app.post("/auth/login", userController.login);
+
+app.patch("/role", userController.roleChange);
+
+app.delete("/user", userController.deleteUser);
+
+const start = async () => {
+  try {
+    await app.listen(PORT, () => {
+      console.log("Server started on " + PORT);
+    });
+    await mongoose.connect(url, () => {
+      console.log("DB IS OK");
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 start();

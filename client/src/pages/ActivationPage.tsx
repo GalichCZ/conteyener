2b750705ebfcf4d75af315f5f1c342dc42996a08
Daniil Galitskii <galichmsk1515@gmail.com>
2022-12-19 16:28:40 +0100

    activation fix

---
+++ client/src/pages/ActivationPage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const ActivationPage = () => {
+  const activationHandler = async () => {};
+
+  return <section className="activation-page">ActivationPage</section>;
+};

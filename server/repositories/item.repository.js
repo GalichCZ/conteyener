d31f709e320365fc5575eaaf35abede940d21339
Daniil Galitskii <daniil.g@byallmeans.work>
2023-12-23 15:04:13 +0100

    added script to change time for all dates

--- server/repositories/item.repository.js
+++ server/repositories/item.repository.js
@@ -3,6 +3,35 @@ const {SendBotMessage} = require("../service/bot-service");
 const dayjs = require("dayjs");
 
 class ItemRepository {
+    async getAllItems() {
+        try {
+            return await ItemSchema.find().exec();
+        } catch (error) {
+            await SendBotMessage(
+                `${dayjs(new Date()).format(
+                    "MMMM D, YYYY h:mm A"
+                )}\nGETTING ITEM ERROR:\n${error}`
+            );
+            console.log(error);
+            return null;
+        }
+    }
+
+    async updateItemById(id,query) {
+        try {
+            await ItemSchema.findByIdAndUpdate(id, query).exec();
+            return true;
+        } catch (error) {
+            await SendBotMessage(
+                `${dayjs(new Date()).format(
+                    "MMMM D, YYYY h:mm A"
+                )}\nGETTING ITEM ERROR:\n${error}`
+            );
+            console.log(error);
+            return error;
+        }
+    }
+
     async getSpecificItem(query){
         try {
             return await ItemSchema.find(query).exec();
